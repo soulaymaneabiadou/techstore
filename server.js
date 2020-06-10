29d006a1a12d4a575bc3748fb50af8d283e75ded
Soulaymane Abiadou <abiadou.soulaymane@gmail.com>
2020-06-10 22:18:18 +0100

    Added static file serving for live preview purposes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
@@ -18,6 +19,13 @@ app.use(fileUpload());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
+}
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 app.use('/api/v1/auth', require('./routes/auth'));
